534f48453de6bb47a415d42d4990a7d024c8ab7c
Raiid Ahmed <raiidahmed@gmail.com>
2023-04-11 13:12:10 -0400

    put desc parsing in its own function Added parse address code
    put desc parsing in its own function
    Added parse address code

--- Clean Final/main.py
+++ Clean Final/main.py
@@ -2,9 +2,24 @@ import pandas as pd
 from datetime import datetime
 import re
 import dateparser as dp
+import usaddress
 import openai
 
 #openai.api_key =
+
+def parse_address(row):
+    addr = usaddress.tag(row[5])
+    print(row[5])
+    try:
+        addr[0]
+        values = list(addr[0].values())
+        value_str = ', '.join(map(str, values))
+        print(value_str)
+
+    except KeyError:
+        print('error')
+
+    return row
 
 def remove_duplicates_and_non_alphanumeric(text):
     words = re.findall(r'\b\w+\b', text)
@@ -67,6 +82,12 @@ def parse_date(row):
     except ValueError:
         print(f"Invalid input format: '{row[0]}'. Skipping this value.")
         return row
+def parse_desc(row):
+    index = row[6].find("eTicket")
+    if index != -1:
+        row[6] = row[6][index + len("eTicket"):]
+
+
 
 # Set your CSV filenames here
 input_csv = 'climate_tech_eventbrite.csv'
@@ -111,27 +132,7 @@ df.iloc[:, 4] = [end for _, end in start_end]
 df.iloc[:, 5] = df.iloc[:, 5].apply(lambda x: remove_duplicates_and_non_alphanumeric(x) if isinstance(x, str) else x)
 
 #remove garbage text from description
-for i in range(len(df.iloc[:, 6])):
-    index = df.iloc[i, 6].find("eTicket")
-    if index != -1:
-        df.iloc[i, 6] = df.iloc[i, 6][index + len("eTicket"):]
-    print(df.iloc[i, 6])
-
-    """response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",  # maybe put in an exception to go to another model in case of rate limiting?
-        messages=[
-            {"role": "system",
-             "content": "You are cleaning the description from eventbrite data sourced from a web scraper. DO NOT SUMMARIZE THE TEXT! You must preserve as much of the original language as possible, just remove any unreadable artifacts, unrelated artifacts, or noise in this text."},
-            {"role": "system", "content": f"Here is the description: {df.iloc[i, 6]}"},
-        ],
-    )
-
-    df.iloc[i,6] = cleaned_text = response['choices'][0]['message']['content']
-    """
-
-    print(df.iloc[i,6])
-
-
+df = df.apply(parse_desc, axis=1)
 
 #Add AM and PM labels to the start time if they are not present
 df = df.apply(append_am_pm, axis=1)
@@ -140,7 +141,7 @@ df = df.apply(append_am_pm, axis=1)
 df = df.apply(parse_date, axis=1)
 
 #Parse addresses
-#df = df.apply(parse_address, axis=1)
+df = df.apply(parse_address, axis=1)
 
 # Save the modified dataframe to output.csv
 df.to_csv('output - NO AI.csv', index=False)
